a9bb696b40f03cdef72d8110480528784a5ea01d
Mohammad-Shrani <mohammad.shirani.developer@gmail.com>
2025-06-15 11:08:48 +0330

    Create and implemnted product page

---
+++ src/pages/product/Product.tsx
@@ -0,0 +1,47 @@
+import { useParams } from "react-router-dom";
+import Container from "../../components/container/Container";
+
+const Product = () => {
+  const params = useParams();
+
+  return (
+    <div>
+      <Container>
+        <div className="h-96 shadow mt-4 grid grid-cols-12">
+          <div className=" col-span-10 p-4">
+            <h1 className="text-right">عنوان محصول</h1>
+            <div>
+              <p className="text-right">قیمت:25$</p>
+              <p className="text-right">
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+                کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
+                جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را
+                برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در
+                زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و
+                دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و
+                زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
+                پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+              </p>
+            </div>
+          </div>
+
+          <div className=" col-span-2 p-4 bg-sky-200">
+            <img
+              className="rounded"
+              src="https://media.istockphoto.com/id/1427276149/vector/abstract-arrow-direction-technology-background.jpg?s=612x612&w=0&k=20&c=KQKxMDhnPGq1-idJW4b67ZgJTeZeMsIp3B9R4PTgeWE="
+              alt=""
+            />
+            <div>
+              <button>Add to Cart</button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Product;
--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Container from "../../components/container/Container";
 import ProductItem from "../../components/productItem/ProductItem";
 
@@ -6,15 +7,30 @@ const Store = () => {
     <div>
       <Container>
         <h1 className="text-right mt-5">جدیدترین محصولات</h1>
-        <div className="grid grid-cols-4 gap-4 mt-4">
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
+        <div className="grid grid-cols-4 gap-4 mt4">
+          <Link to={`/product/${1}`}>
+            <ProductItem />
+          </Link>
+
+          <Link to={`/product/${2}`}>
+            <ProductItem />
+          </Link>
+
+          <Link to={`/product/${3}`}>
+            <ProductItem />
+          </Link>
+
+          <Link to={`/product/${4}`}>
+            <ProductItem />
+          </Link>
+
+          <Link to={`/product/${5}`}>
+            <ProductItem />
+          </Link>
+
+          <Link to={`/product/${6}`}>
+            <ProductItem />
+          </Link>
         </div>
       </Container>
     </div>
