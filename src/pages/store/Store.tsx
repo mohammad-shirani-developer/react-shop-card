a444c6c188ab42b101d40362f2fdc90a3c59b922
Mohammad-Shrani <mohammad.shirani.developer@gmail.com>
2025-06-17 11:28:13 +0330

    Fetch data with axisos

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,36 +1,27 @@
 import { Link } from "react-router-dom";
 import Container from "../../components/container/Container";
 import ProductItem from "../../components/productItem/ProductItem";
+import { useEffect, useState } from "react";
+import { getProducts } from "../../services/api-client";
 
 const Store = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getProducts().then((res) => {
+      setProducts(res);
+    });
+  }, []);
+
   return (
     <div>
       <Container>
         <h1 className="text-right mt-5">جدیدترین محصولات</h1>
         <div className="grid grid-cols-4 gap-4 mt4">
-          <Link to={`/product/${1}`}>
-            <ProductItem />
-          </Link>
-
-          <Link to={`/product/${2}`}>
-            <ProductItem />
-          </Link>
-
-          <Link to={`/product/${3}`}>
-            <ProductItem />
-          </Link>
-
-          <Link to={`/product/${4}`}>
-            <ProductItem />
-          </Link>
-
-          <Link to={`/product/${5}`}>
-            <ProductItem />
-          </Link>
-
-          <Link to={`/product/${6}`}>
-            <ProductItem />
-          </Link>
+          {products.map((item) => (
+            <Link to={`/product/${1}`}>
+              <ProductItem />
+            </Link>
+          ))}
         </div>
       </Container>
     </div>
